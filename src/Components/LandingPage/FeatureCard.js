207d057485d72ba7c14d7d7c89e9ea0d5d1f60b4
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2020-05-24 17:30:44 -0400

    Code cleanup.

--- src/Components/LandingPage/FeatureCard.js
+++ src/Components/LandingPage/FeatureCard.js
@@ -1,14 +1,14 @@
 import React, {Component} from "react";
 import ReactCardFlip from 'react-card-flip';
-import {makeStyles} from "@material-ui/core/styles";
 import CardFront from "../Images/feature_card_front.png";
 import CardBack from "../Images/feature_card_back.png";
+import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   image: { 
-    position: 'relative', 
+    position: 'relative',
     textAlign: 'center',
-    color: 'white',
+    margin: '10px',
   },
   front_text: { 
     position: 'absolute',
@@ -17,20 +17,23 @@ const useStyles = makeStyles(theme => ({
     width: '100%',
     paddingLeft: '20px',
     paddingRight: '20px',
+    color: 'white',
   },
   small_text: { 
     position: 'absolute',
     marginTop: '60%',
     fontSize: '1vw',
     width: '100%',
+    color: 'white',
   },
   back_text: { 
     position: 'absolute',
-    marginTop: '20%',
     fontSize: '1vw',
     width: '100%',
+    height: '100%',
     paddingLeft: '10px',
     paddingRight: '10px',
+    color: 'black',
   },
 }));
 
@@ -59,15 +62,15 @@ class FeatureCard extends Component{
     const classes = this.props.classes;
     return (
       <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-        <div className={classes.image} style={{margin: '20px'}} onClick={this.handleClick}>
+        <div className={classes.image} onClick={this.handleClick}>
           <h2 className={classes.front_text}>{this.props.front_text}</h2>
           <p className={classes.small_text}>more text on the flip side</p>
           <img style={{width: '100%', height: '100%'}} src={CardFront} alt="Front of feature card"/>
         </div>
  
-        <div className={classes.image} style={{margin: '20px'}} onClick={this.handleClick}>
-          <img style={{width: '100%', height: '100%'}} src={CardBack} alt="Back of feature card"/>
+        <div className={classes.image} onClick={this.handleClick}>
           <p className={classes.back_text}>{this.props.back_text}</p>
+          <img style={{width: '100%', height: '100%'}} src={CardBack} alt="Back of feature card"/>
         </div>
       </ReactCardFlip>
     )
--- src/Components/LandingPage/Features.js
+++ src/Components/LandingPage/Features.js
@@ -1,12 +1,39 @@
 import React, {Component} from "react";
 import Grid from "@material-ui/core/Grid";
 import FeatureCard from "./FeatureCard.js"
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  background: { 
+    color: '#F1F1F1',
+  },
+  features_title: { 
+    paddingTop: '30px',
+    fontWeight: '500',
+    fontSize: '36px',
+    color: 'black',
+  },
+  grid: { 
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
+
+function withMyHook(Component){
+    return function WrappedComponent(props){
+        const classes = useStyles();
+        return <Component {...props} classes={classes}/>
+    }
+}
 
 class Features extends Component{
   render() {
+    const classes = this.props.classes;
     return(
-      <div style={{backgroundColor: '#F1F1F1'}}>
-        <h1 style={{paddingTop: '30px'}}>Features</h1>
+      <div className={classes.background}>
+        <h1 className={classes.features_title}>Features</h1>
         <Grid
           container
           spacing={1}
@@ -20,10 +47,19 @@ class Features extends Component{
             alignItems="center"
             justify="center"
           >
-            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-              <FeatureCard front_text="Exclusive Coffee Chat Series" back_text="To be determined"/>
-              <FeatureCard front_text="Professional Career & Board Opportunities" back_text="To be determined"/>
-              <FeatureCard front_text="Hire MAX Professional Talent" back_text="To be determined"/>
+            <div className={classes.grid}>
+              <FeatureCard
+                front_text="Exclusive Coffee Chat Series"
+                back_text="To be determined"
+              />
+              <FeatureCard
+                front_text="Professional Career & Board Opportunities"
+                back_text="To be determined"
+              />
+              <FeatureCard
+                front_text="Hire MAX Professional Talent"
+                back_text="To be determined"
+              />
             </div>
           </Grid>
           <Grid
@@ -33,10 +69,19 @@ class Features extends Component{
             alignItems="center"
             justify="center"
           >
-            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-              <FeatureCard front_text="Resume Bank" back_text="To be determined"/>
-              <FeatureCard front_text="MAX Events" back_text="To be determined"/>
-              <FeatureCard front_text="MAX Resources" back_text="To be determined"/>
+            <div className={classes.grid}>
+              <FeatureCard
+                front_text="Resume Bank"
+                back_text="To be determined"
+              />
+              <FeatureCard
+                front_text="MAX Events"
+                back_text="To be determined"
+              />
+              <FeatureCard
+                front_text="MAX Resources"
+                back_text="To be determined"
+              />
             </div>
           </Grid>
         </Grid>
@@ -45,4 +90,5 @@ class Features extends Component{
   }
 }
 
+Features = withMyHook(Features);
 export default Features;
