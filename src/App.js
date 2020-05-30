8c02c83385145724735c9a445484962139001273
mohaimen.khan@mail.utoronto.ca <mohaimen.khan@mail.utoronto.ca>
2020-05-29 23:17:16 -0400

    Renamed to signin since that's the page I will be working on

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Landing from "./Components/LandingPage/Landing";
+import SignIn from "./Components/Authentication/SignIn";
 import './App.css';
 
 class App extends Component{
@@ -12,7 +13,7 @@ class App extends Component{
 
   componentDidMount() {
     this.setState({
-      currentScreen: <Landing appContext={this}/>
+      currentScreen: <SignIn appContext={this}/>
     })
   }
 
