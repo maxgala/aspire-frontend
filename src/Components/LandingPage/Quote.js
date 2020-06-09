5d9a8006102a9894f3de2b69f29810dee3026afd
Fatum Abdullahi <f2abdull@uwaterloo.ca>
2020-06-09 01:10:56 -0400

    added change of screen functionality

--- src/Components/LandingPage/Quote.js
+++ src/Components/LandingPage/Quote.js
@@ -1,17 +1,91 @@
 import React, { useState } from 'react';
 import {Component} from "react";
 import { css, cx } from 'emotion'
+import { makeStyles } from "@material-ui/core/styles";
+import aboutMax from "../Images/aboutMax.png";
+import Grid from "@material-ui/core/Grid";
+function withMyHook(Component){
+  return function WrappedComponent(props){
+    const classes = useStyles();
+    return <Component {...props} classes={classes}/>
+  }
+}
+const useStyles = makeStyles(theme => ({
+  h1style: {
+    position: 'relative',
+    color: "black",
+    fontSize: "20px",
+    marginBottom: "0",
+    marginTop: "0",
+    letterSpacing: "0px",
+    padding: '5%'
+  },
+  f1:{
+    fontFamily: "Bold 36px/48px Nunito Sans;",
+    textAlign: 'center',
+  },
+  f2:{
+    fontFamily: "Regular 18px/24px Montserrat",
+    maxWidth: '450px',
+    textAlign: 'center',
+  },
+  f3:{
+    fontFamily: "Bold 26px/36px Nunito Sans",
+    marginLeft: "1em",
+    maxWidth: '350px',
+    textAlign: 'left',
+  },
+   f4:{
+    fontFamily: "Regular 10px/12px Montserrat",
+    maxWidth: '450px',
+    textAlign: 'center',
+    fontSize:"10px",
+  },
+  tab: { 
+    display: "flex",
+    alignItems:"center",
+    justifyContent:"top",
+    fontFamily: "Bold 36px/48px Nunito Sans;",
+    verticalAlign: "middle",
+  },
+  about: {
+    display: 'grid',
+    height:'fit-content',
+    width: "100vw",
+    minHeight: "75vh",
+    overflow: "hidden",
+    marginRight : "0",
+    marginLeft  : "0",
+  },
+  total: {
+    width: '100%',
+    height: '60vh',
+    padding: '5%',
+  },
+  grid: { 
+    paddingLeft: '10%',
+    paddingRight: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
 const quotes={
   0:{
-    client:"Emma Stone",
-    quote:<img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sunflower-1508785046.jpg"/>
+    
+    client:"\"When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.\"",
+    quote:<img className={css`
+    width: 180%;
+    height:80%;
+    padding:2%;
+    border-radius: 50%;
+    
+   `} src="https://upload.wikimedia.org/wikipedia/commons/f/f6/Sean_Cunningham_Head_Shot.jpg"/>
   },
    1:{
     client:"Coco",
     quote:"In order to be irreplaceable one must always be different."
   },
    2:{
-    client:"Margaret Mead",
     quote:"Always remember that you are absolutely unique. Just like everyone else"
   },
    3:{
@@ -20,6 +94,7 @@ const quotes={
   },
 }
 class Quote extends Component{
+ 
   constructor(props){
     super(props);
     this.state={
@@ -39,25 +114,64 @@ class Quote extends Component{
     this.setActive(event.target.getAttribute("data-quote"));
   }
   render(){
-   
+
 
   return(
-    <div className={css`
+      <div className={css`
+      align-items:center;
+      margin:40px auto;`}>
+  <h1 className={css`
+      font-family: Nunito Sans;
+      font-size:40px;
+      font-weight:bolder;
+      text-align: center;
+      `}
+      
+      
+      style={{  paddingTop: '30px'}} ><b>What our members have to say</b></h1>
+    <Grid
+    container
+    item
+   // spacing={1}
+    alignItems="center"
+    justify="center"
+    className={css ` `}
+  >
+    <Grid
+      container
+      item xs={4} sm={4}md={3} lg={2}
+
+      //alignItems="center"
+      justify="left"
    
+    >
+          <p className={css`
+            width: 50%;
+            height:37vh;
+           
+          
+            border-radius: 50%;
+         
+          `}> 
+            {this.state.current.quote}    
+          </p>
+    </Grid>
+    <Grid
+      container
+      item  xs={12} sm={12} md={4} lg={3}
 
-    align-items:center;
-    margin:40px auto;
-    max-width:700px;
-    p{
-      text-alighn:center;
-      margin-bottom:20px;
-      color:pink;
-    }
-
-    `}>
-    <p>{this.state.current.quote}</p>
-     <p>{this.state.current.client}</p>
-     <div className={css`
+     alignItems="first baseline"
+      justify="center"
+    >
+      
+      < p className={css` 
+      font-size:25px;`}><i>{this.state.current.client}</i></p>
+      < p className={css` 
+      font-size:20px;`}><i><b>Jodie Foster</b></i></p>
+      
+    </Grid>
+  </Grid>
+  <div className={css`
      display:flex;
      justify-content:center;
      span{
@@ -92,10 +206,12 @@ class Quote extends Component{
         key={index}
         />
         ))}
-     </div>
-    
 
-    </div>
+</div>
+
+  </div>
+
+
     )
 }
 }
