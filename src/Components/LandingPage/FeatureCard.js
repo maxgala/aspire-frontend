99987dc97d4c67d074fd3b6c60844d839ebd9226
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2020-06-17 18:53:57 -0400

    Fixed text position and styling on top of images.

--- src/Components/LandingPage/FeatureCard.js
+++ src/Components/LandingPage/FeatureCard.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles(theme => ({
     position: 'relative',
     textAlign: 'center',
     marginLeft: '10px',
-    marginRight: '10px'
+    marginRight: '10px',
   },
   front_text: { 
     position: 'absolute',
@@ -35,14 +35,17 @@ const useStyles = makeStyles(theme => ({
     color: 'white',
   },
   back_text: { 
-    position: 'absolute',
-    top: '40%',
-    transform: 'translate(0%, -50%)',
     fontSize: '20px',
     width: '100%',
-    paddingLeft: '15px',
-    paddingRight: '15px',
+    marginTop: '30%',
+    paddingLeft: '20px',
+    paddingRight: '20px',
     color: 'white',
+  },
+  centered: {
+    position: 'absolute',
+    width: '100%',
+    top: '0%',
   }
 }));
 
@@ -73,16 +76,20 @@ class FeatureCard extends Component{
     return (
       <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
         <div className={classes.image} onClick={this.handleClick} onMouseEnter={this.handleClick} onMouseLeave={this.handleClick}>
-          <h2 className={classes.front_text}>{this.props.front_text}</h2>
-          <p className={classes.small_text}>more text on the flip side</p>
           <img style={{width: '100%', height: '100%', opacity: '1', marginBottom: '-101%'}} src={tint} alt="Tint on front of feature card"/>
           <img style={{width: '100%', height: '100%', opacity: '0.2'}} src={this.state.images[this.props.card_number - 1]} alt="Front of feature card"/>
+          <div className={classes.centered}>
+            <h2 className={classes.front_text}>{this.props.front_text}</h2>
+            <p className={classes.small_text}>more text on the flip side</p>
+          </div>
         </div>
  
         <div className={classes.image} onClick={this.handleClick} onMouseEnter={this.handleClick} onMouseLeave={this.handleClick}>
-          <p className={classes.back_text}>{this.props.back_text}</p>
           <img style={{width: '100%', height: '100%', opacity: '1', marginBottom: '-101%'}} src={tint} alt="Tint on front of feature card"/>
           <img style={{width: '100%', height: '100%', opacity: '0.2'}} src={this.state.images[this.props.card_number - 1]} alt="Back of feature card"/>
+          <div className={classes.centered}>
+            <p className={classes.back_text}>{this.props.back_text}</p>
+          </div>
         </div>
       </ReactCardFlip>
     )
