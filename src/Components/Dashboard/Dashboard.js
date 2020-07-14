7d52fcbb0aa5659bb6cafd4db9ab4d139d3af96a
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2020-07-13 20:17:27 -0400

    Fixed issue with isSeniorExec prop being lost when pages switch.

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -138,19 +138,19 @@ class Dashboard extends Component {
 
   changeToCoffeeChats() {
     this.setState({
-      currentScreen: <CoffeeChats appContext={this}/>
+      currentScreen: <CoffeeChats appContext={this} isSeniorExec={this.props.isSeniorExec}/>
     }) 
   }
 
   changeToJobs() {
     this.setState({
-      currentScreen: <Jobs appContext={this}/>
+      currentScreen: <Jobs appContext={this} isSeniorExec={this.props.isSeniorExec}/>
     }) 
   }
 
   changeToDashboard() {
     this.setState({
-      currentScreen: <Home appContext={this}/>
+      currentScreen: <Home appContext={this} isSeniorExec={this.props.isSeniorExec}/>
     }) 
   }
 
