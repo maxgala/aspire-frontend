2493ea002b58311d6bbcc973b4b18c2c796c449b
Shehryar Assad <shehryar.assad@cookie.ai>
2020-07-25 13:25:23 -0400

    Add AWS cognito config. Create submit form function in Register.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React, {Component} from 'react';
-import Landing from "./Components/LandingPage/Landing";
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Amplify from "aws-amplify";
+import credentials from "./aws-exports";
 
-class App extends Component{
-  constructor(props){
+Amplify.configure(credentials);
+
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      currentScreen: []
-    }
+      currentScreen: [],
+    };
   }
 
-  componentDidMount() {
-    this.setState({
-      currentScreen: <Landing appContext={this}/>
-    })
-  }
-
-  render(){
-    return (
-        <div className="App">
-          {this.state.currentScreen}
-        </div>
-    );
+  render() {
+    return <div className="App">{this.state.currentScreen}</div>;
   }
 }
 
