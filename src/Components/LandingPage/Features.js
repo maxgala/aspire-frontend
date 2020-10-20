5b44e095ebd73f993e9b42391d39f39d75824464
Waleed <w41eed@outlook.com>
2020-10-20 14:16:25 -0400

    did some minor changes to grid

--- src/Components/LandingPage/Features.js
+++ src/Components/LandingPage/Features.js
@@ -7,7 +7,10 @@ const useStyles = makeStyles(() => ({
   background: { 
     backgroundColor: '#F1F1F1',
     paddingTop:'1%',
-    paddingBottom:'5%',
+    paddingBottom:'10%',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+  
   },
   features_title: { 
     fontFamily: "Nunito Sans",
@@ -17,11 +20,11 @@ const useStyles = makeStyles(() => ({
     color: 'black',
   },
   grid: { 
-    paddingLeft: '10%',
-    paddingRight: '10%',
+    paddingLeft: '0%',
+    paddingRight: '0%',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingBottom: '30px',
+    
   },
 }));
 
@@ -40,7 +43,8 @@ class Features extends Component{
         <h1 className={classes.features_title}>Features</h1>
         <Grid
           container
-          spacing={1}
+          spacing={2}
+          direction='row'
           alignItems="center"
           justify="center"
           className={classes.grid}
@@ -48,8 +52,8 @@ class Features extends Component{
         >  
           <Grid
             container
-            item xs={12} sm={9} md={6} lg={true}
-            spacing={2}
+            item xs={12} sm={9} md={6} lg={4}
+            spacing={1}
             alignItems="center"
             justify="center"
           >
@@ -61,8 +65,8 @@ class Features extends Component{
           </Grid>
           <Grid
             container
-            item xs={12} sm={9} md={6} lg={true}
-            spacing={2}
+            item xs={12} sm={9} md={6} lg={4}
+            spacing={1}
             alignItems="center"
             justify="center"
           >
@@ -74,8 +78,8 @@ class Features extends Component{
           </Grid>
           <Grid
             container
-            item xs={12} sm={9} md={6} lg={true}
-            spacing={2}
+            item xs={12} sm={9} md={6} lg={4}
+            spacing={1}
             alignItems="center"
             justify="center"
           >
@@ -87,8 +91,8 @@ class Features extends Component{
           </Grid>
           <Grid
             container
-            item xs={12} sm={9} md={6} lg={true}
-            spacing={2}
+            item xs={12} sm={9} md={6} lg={4}
+            spacing={1}
             alignItems="center"
             justify="center"
           >
@@ -100,8 +104,8 @@ class Features extends Component{
           </Grid>
           <Grid
             container
-            item xs={12} sm={9} md={6} lg={true}
-            spacing={2}
+            item xs={12} sm={9} md={6} lg={4}
+            spacing={1}
             alignItems="center"
             justify="center"
           >
@@ -113,8 +117,8 @@ class Features extends Component{
           </Grid>
           <Grid
             container
-            item xs={12} sm={9} md={6} lg={true}
-            spacing={2}
+            item xs={12} sm={9} md={6} lg={4}
+            spacing={1}
             alignItems="center"
             justify="center"
           >
