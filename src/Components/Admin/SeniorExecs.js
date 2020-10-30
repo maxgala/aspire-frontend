6fd9a71b4a6b943b07e8919d8cccf7203a7a9cf3
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2020-10-29 22:17:05 -0400

    Revert senior exec page deletion.

---
+++ src/Components/Admin/SeniorExecs.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({}));
+
+function withMyHook(Component) {
+  return function WrappedComponent(props) {
+    const classes = useStyles();
+    return <Component {...props} classes={classes} />;
+  };
+}
+
+class SeniorExecs extends Component {
+  render() {
+    return (
+      <div>
+        <p>Senior Execs</p>
+      </div>
+    );
+  }
+}
+
+SeniorExecs = withMyHook(SeniorExecs);
+export default SeniorExecs;
