c0f34e6ce2811d44a25101ec54b25af8fba52e4c
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2020-11-07 12:26:28 -0500

    Hide filters on various dashboard pages.

--- src/Components/Dashboard/ResumeBank.js
+++ src/Components/Dashboard/ResumeBank.js
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Filter from "./Cards/FilterCard";
 // import PerfectScrollbar from "@opuscapita/react-perfect-scrollbar";
 import ResumeBankCard from "./Cards/ResumeBankCard";
 import TestData from "./CoffeeChatsTestData";
+// TODO: Hiding filters until they get implemented
+// import Filter from "./Cards/FilterCard";
 
 const useStyles = makeStyles(() => ({
   mainPage: {
-    paddingLeft: '8%',
-    paddingRight: '8%',
-    paddingBottom:'8%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '90vh',
+    paddingLeft: "8%",
+    paddingRight: "8%",
+    paddingBottom: "8%",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "90vh",
   },
 
   JobBoard: {
-    fontFamily: 'PT Sans',
-    fontSize: '30px',
-    textAlign: 'left',
-    color: '#58595B',
-    fontWeight: 'bold',
-    marginTop: '40px',
+    fontFamily: "PT Sans",
+    fontSize: "30px",
+    textAlign: "left",
+    color: "#58595B",
+    fontWeight: "bold",
+    marginTop: "40px",
   },
 
   grid: {
@@ -30,16 +31,14 @@ const useStyles = makeStyles(() => ({
     alignItems: "center",
   },
 
-  
-
   section_title: {
-    fontFamily: 'PT Sans',
-    fontSize: '15px',
-    margin: '5px',
-    marginBottom: '10px',
-    textAlign: 'left',
-    color: 'black',
-    fontWeight: 'bold',
+    fontFamily: "PT Sans",
+    fontSize: "15px",
+    margin: "5px",
+    marginBottom: "10px",
+    textAlign: "left",
+    color: "black",
+    fontWeight: "bold",
   },
 
   select: {
@@ -57,11 +56,10 @@ const useStyles = makeStyles(() => ({
   },
 
   date: {
-    fontFamily: 'PT Sans',
-    fontSize: '15px',
-    fontWeight: 'bold',
+    fontFamily: "PT Sans",
+    fontSize: "15px",
+    fontWeight: "bold",
   },
-
 }));
 
 function withMyHook(Component) {
@@ -84,146 +82,155 @@ class JobBoard extends Component {
     return (
       <div>
         {/* <PerfectScrollbar> */}
-          <div className={classes.mainPage}>
-          
-              <h1 className={classes.JobBoard}>Resume Bank</h1>
+        <div className={classes.mainPage}>
+          <h1 className={classes.JobBoard}>Resume Bank</h1>
 
+          {/* TODO: Hiding filters until they get implemented
+          <Grid
+            container
+            item
+            xs={12}
+            spacing={1}
+            alignItems="flex-start"
+            justify="flex-start"
+          >
             <Grid
               container
               item
               xs={12}
+              sm={6}
+              md={6}
+              lg={3}
               spacing={1}
-              alignItems="flex-start"
-              justify="flex-start"
+              alignItems="center"
+              justify="center"
             >
               <Grid
                 container
                 item
                 xs={12}
-                sm={6}
-                md={6}
-                lg={3}
                 spacing={1}
-                alignItems="center"
-                justify="center"
+                alignItems="flex-start"
+                justify="flex-start"
               >
-                <Grid
-                  container
-                  item
-                  xs={12}
-                  spacing={1}
-                  alignItems="flex-start"
-                  justify="flex-start"
-                >
-                  <p className={classes.section_title}>Job Title</p>
-                </Grid>
-                <Grid
-                  container
-                  item
-                  xs={12}
-                  spacing={1}
-                  alignItems="flex-start"
-                  justify="flex-start"
-                >
-                  <Filter />
-                </Grid>
+                <p className={classes.section_title}>Job Title</p>
               </Grid>
               <Grid
                 container
                 item
                 xs={12}
-                sm={6}
-                md={6}
-                lg={3}
                 spacing={1}
-                alignItems="center"
-                justify="center"
+                alignItems="flex-start"
+                justify="flex-start"
               >
-                <Grid
-                  container
-                  item
-                  xs={12}
-                  spacing={1}
-                  alignItems="flex-start"
-                  justify="flex-start"
-                >
-                  <p className={classes.section_title}>Location</p>
-                </Grid>
-                <Grid
-                  container
-                  item
-                  xs={12}
-                  spacing={1}
-                  alignItems="flex-start"
-                  justify="flex-start"
-                >
-                  <Filter />
-                </Grid>
-              </Grid>
-
-              <Grid
-                container
-                item xs={12} sm={6} md={6} 
-                spacing={1}
-                alignItems="center"
-                justify="center"
-              >
-                <Grid
-                  container
-                  item
-                  xs={12}
-                  spacing={1}
-                  alignItems="flex-start"
-                  justify="flex-start"
-                >
-                  <p className={classes.section_title}>Additional Filters</p>
-                </Grid>
-                <Grid
-                  container
-                  item
-                  xs={12}
-                  spacing={1}
-                  alignItems="flex-start"
-                  justify="flex-start"
-                >
-                  <Filter />
-                </Grid>
+                <Filter />
               </Grid>
             </Grid>
-
-            <div className={classes.sort}>
-              <p className={classes.date}>
-                {" "}
-                Sort date posted by:
-                <select className={classes.select}>
-                  <option value="Ascending">Ascending</option>
-                  <option value="descending">Descending</option>
-                </select>
-              </p>
-            </div>
+            <Grid
+              container
+              item
+              xs={12}
+              sm={6}
+              md={6}
+              lg={3}
+              spacing={1}
+              alignItems="center"
+              justify="center"
+            >
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={1}
+                alignItems="flex-start"
+                justify="flex-start"
+              >
+                <p className={classes.section_title}>Location</p>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={1}
+                alignItems="flex-start"
+                justify="flex-start"
+              >
+                <Filter />
+              </Grid>
+            </Grid>
 
             <Grid
               container
               item
               xs={12}
+              sm={6}
+              md={6}
               spacing={1}
-              alignItems="flex-start"
-              justify="flex-start"
+              alignItems="center"
+              justify="center"
             >
-              {this.state.job_board_data.map((chat, key) => (
-                <Grid
-                  key={key}
-                  container
-                  item xs={12} sm={12} md={6} lg={4} xl={3}
-                  spacing={1}
-                  alignItems="flex-start"
-                  justify="flex-start"
-                >
-                  <ResumeBankCard data={chat} />
-                </Grid>
-              ))}
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={1}
+                alignItems="flex-start"
+                justify="flex-start"
+              >
+                <p className={classes.section_title}>Additional Filters</p>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                spacing={1}
+                alignItems="flex-start"
+                justify="flex-start"
+              >
+                <Filter />
+              </Grid>
             </Grid>
+          </Grid>
+
+          <div className={classes.sort}>
+            <p className={classes.date}>
+              {" "}
+              Sort date posted by:
+              <select className={classes.select}>
+                <option value="Ascending">Ascending</option>
+                <option value="descending">Descending</option>
+              </select>
+            </p>
           </div>
+          */}
+
+          <Grid
+            container
+            item
+            xs={12}
+            spacing={1}
+            alignItems="flex-start"
+            justify="flex-start"
+          >
+            {this.state.job_board_data.map((chat, key) => (
+              <Grid
+                key={key}
+                container
+                item
+                xs={12}
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+                spacing={1}
+                alignItems="flex-start"
+                justify="flex-start"
+              >
+                <ResumeBankCard data={chat} />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
         {/* </PerfectScrollbar> */}
       </div>
     );
