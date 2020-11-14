f1c49e112a84d40ae581ef85d63a00ec6c97d1a4
ahaq0 <ammarhaq@outlook.com>
2020-11-13 22:08:14 -0800

    Added Admin Dashboard.

--- src/Components/Admin/JobPosts.js
+++ src/Components/Admin/JobPosts.js
@@ -1,17 +1,47 @@
 import React, {Component} from 'react';
-import {withStyles, makeStyles} from "@material-ui/core/styles";
-import PerfectScrollbar from "@opuscapita/react-perfect-scrollbar";
-import Grid from "@material-ui/core/Grid";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { makeStyles} from "@material-ui/core/styles";
+import { httpGet, httpPut } from "../../lib/dataAccess";
+import MaterialTable from 'material-table';
+import { forwardRef } from 'react';
+
+import { AddBox , ArrowUpward} from "@material-ui/icons";
+
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 
-
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
+  
+    
 const useStyles = makeStyles((theme) => ({
   home_page: { 
     paddingLeft: '5%',
@@ -56,25 +86,6 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-
 function withMyHook(Component){
   return function WrappedComponent(props){
     const classes = useStyles();
@@ -82,71 +93,79 @@ function withMyHook(Component){
   }
 }
 
-function createData(name, salary, location, state, country) {
-  return { name, salary, location, state, country };
-}
+class JobPosts extends Component {
 
-const rows = [
-  createData('Senior Waste Collector', 50, 'Surrey', 'BC', 'Canada'),
-  createData('Ice Cream Product Manager', 40, 'Surrey', 'BC', 'Canada'),
-  createData('Chicken Biryani Architect', 200, 'Brampton', 'OT', 'Canada'),
-  createData('Orange Wasteman', 400, "Washington", 'DC', 'USA'),
-];
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: [],
+      columns: [
+        { title: 'Title', field: 'title' },
+        { title: 'Company', field: 'company' },
+        { title: 'Region', field: 'region'},
+        { title: 'City', field: "city"}  
+      ]
+    }
+  }
 
+  fetchJobs = async () => {
+    const existingJobsData = await httpGet("jobs?status=ACTIVE,UNDER_REVIEW", localStorage.getItem("idToken"));
+    this.setState({
+      jobs: existingJobsData.data.jobs
+    })
+  }
 
-class JobPosts extends Component{
+  removeJob = async (jobID) => {
+
+    let removedJob = {
+      "job_status" : "REJECTED",
+    };
+    const removedJobsData = await httpPut(`jobs/${jobID}`, localStorage.getItem("idToken"), removedJob);
+    
+    console.log(removedJobsData);
+    // The job is already removed from state, so we just send the PUT request to remove it
+  }
+
+  componentDidMount() {
+    this.fetchJobs();
+    console.log(this.state);
+  }
+
   render() {
 
-    const classes = this.props.classes;
+  return (
+    <MaterialTable
+      title="Global Job Postings"
+      columns={this.state.columns}
+      icons={tableIcons}
+      data={this.state.jobs}
+      options={{
+        paging:true,
+        pageSize:15,
+        emptyRowsWhenPaging:true,
+        pageSizeOptions:[5,10,15,30,50]
+      }}
+      editable={{
+        onRowDelete: oldData => 
+          new Promise((resolve, reject) => {
 
-    return (
-        <div>
-          <PerfectScrollbar>
-            <div className={classes.mainPage}>
-              <h1 className={classes.JobBoard}>
-                Job Postings
-              </h1>
-              <Grid
-               container
-               item xs={12}
-               spacing={1}
-               alignItems="flex-start"
-               justify="flex-start"
-               className={classes.JobTable}
-               >
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Title</StyledTableCell>
-            <StyledTableCell align="right">Hourly Salary</StyledTableCell>
-            <StyledTableCell align="right">City</StyledTableCell>
-            <StyledTableCell align="right">State / Province</StyledTableCell>
-            <StyledTableCell align="right">Country</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">${row.salary}</StyledTableCell>
-              <StyledTableCell align="right">{row.location}</StyledTableCell>
-              <StyledTableCell align="right">{row.state}</StyledTableCell>
-              <StyledTableCell align="right">{row.country}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-                 
-               </Grid>
 
-            </div>
-          </PerfectScrollbar>
-        </div>
-      );
+            // Make a copy of the jobs in state prior to removing the deleted job
+            const dataDelete = [...this.state.jobs];
+            const index = oldData.tableData.id;
+
+            dataDelete.splice(index, 1);
+            this.setState({
+              jobs: [...dataDelete]
+            });
+            // Set the job status to REJECTED in the DB.
+            this.removeJob(oldData.job_id);
+    
+            resolve();
+          })
+      }}
+    />
+  );
   }
 }
 
