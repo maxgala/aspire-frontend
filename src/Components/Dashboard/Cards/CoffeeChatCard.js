c5cdfc8e34294313737c5287aae44f0d7f06c555
arsidada <arsalan.rana@gmail.com>
2020-11-20 15:43:35 -0500

    fixing coffee chats & some other fixes

--- src/Components/Dashboard/Cards/CoffeeChatCard.js
+++ src/Components/Dashboard/Cards/CoffeeChatCard.js
@@ -379,10 +379,10 @@ class CoffeeChatCard extends Component {
           this.props.data.chat_status === "ChatStatus.RESERVED"
             ? classes.cardBooked
             : this.props.data.chat_type === ChatTypes.oneOnOne
-            ? classes.cardOne
-            : this.props.data.chat_type === ChatTypes.fourOnOne
-            ? classes.cardFour
-            : classes.cardInterview
+              ? classes.cardOne
+              : this.props.data.chat_type === ChatTypes.fourOnOne
+                ? classes.cardFour
+                : classes.cardInterview
         }
       >
         {/* need to get image from s3 bucket --  */}
@@ -431,19 +431,14 @@ class CoffeeChatCard extends Component {
               >
                 <h1 className={classes.title}>
                   {this.props.data.chat_type === ChatTypes.oneOnOne
-                    ? "One-on-One"
+                    ? "One on One"
                     : this.props.data.chat_type === ChatTypes.fourOnOne
-                    ? "Four-on-One"
-                    : "Mock Interview"}
-                  {this.props.data.chat_status === "ChatStatus.PENDING" ? (
-                    <span className={classes.booked}> booked </span>
-                  ) : (
-                    ""
-                  )}
+                      ? "Four on One"
+                      : "Mock Interview"}
                 </h1>
                 <p className={classes.subtitle}>
                   <span className={classes.name}>
-                    {this.props.data.senior_executive}
+                    {this.props.data.given_name} {this.props.data.family_name}
                   </span>{" "}
                   {this.props.data.title}
                 </p>
@@ -454,7 +449,7 @@ class CoffeeChatCard extends Component {
                       className={classes.company_icon}
                     />
                   </span>{" "}
-                  Random Company
+                  {this.props.data["custom:company"]}
                 </span>
 
                 {this.props.data &&
@@ -497,15 +492,20 @@ class CoffeeChatCard extends Component {
                   alignItems="flex-start"
                   justify="flex-start"
                 >
+
                   <span className={classes.button_container}>
-                    <Button
-                      onClick={this.openCoffeeChat}
-                      className={classes.button}
-                      variant="contained"
-                      color="primary"
-                    >
-                      View Booking
-                    </Button>
+                    {this.props.data.chat_status === "ACTIVE" ? (
+                      <Button
+                        onClick={this.openCoffeeChat}
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                      >
+                        View Booking
+                      </Button>
+                    ) : (
+                      ""
+                    )}
                   </span>
                 </Grid>
               </Grid>
@@ -590,18 +590,18 @@ class CoffeeChatCard extends Component {
                   >
                     <h1 className={classes.title2}>
                       {this.props.data.chat_type === ChatTypes.oneOnOne
-                        ? "One-on-One"
+                        ? "One on One"
                         : this.props.data.chat_type === ChatTypes.fourOnOne
-                        ? "Four-on-One"
-                        : "Mock Interview"}
+                          ? "Four on One"
+                          : "Mock Interview"}
                       {this.props.data.booked ? (
                         <span className={classes.booked}>booked</span>
                       ) : (
-                        ""
-                      )}{" "}
+                          ""
+                        )}{" "}
                       with&nbsp;
                       <span className={classes.name2}>
-                        {this.props.data.senior_executive}
+                        {this.props.data.given_name} {this.props.data.family_name}
                       </span>
                     </h1>
                   </Grid>
@@ -615,7 +615,7 @@ class CoffeeChatCard extends Component {
                   >
                     <span className={classes.subtitle2}>
                       <span>{this.props.data.senior_executive} @ </span>
-                      {this.props.data.company} Random Company
+                      {this.props.data["custom:company"]}
                     </span>
                   </Grid>
                   <Grid
