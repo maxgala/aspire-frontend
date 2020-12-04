bae808721d1ae2467e4b5ecbb8e579640cd95cfd
arsidada <arsalan.rana@gmail.com>
2020-12-04 03:11:23 -0500

    removing unused variable

--- src/Components/Dashboard/Cards/CoffeeChatCard.js
+++ src/Components/Dashboard/Cards/CoffeeChatCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import blankProfile from "../../Images/faceShot/blank_profile.png";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import { faBuilding } from "@fortawesome/free-solid-svg-icons";
