0f9f8c5684a431798dc10858d3f3f5820804fce4
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2020-12-11 01:18:51 -0500

    Fix post job functionality. (#282)
    * Fix post job functionality.
    
    * prefered way to do async functions.
    
    * Async post request.
    
    * Hide post a job button to free users.
    
    * remove console logs.

--- src/Components/Dashboard/UserProfile.js
+++ src/Components/Dashboard/UserProfile.js
@@ -128,7 +128,6 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "auto",
     display: "block",
     objectFit: "cover",
-
   },
 
   button: {
@@ -382,11 +381,13 @@ class Landing extends Component {
       .getIdToken()
       .getJwtToken()
       .toString();
+
     await httpGet("jobs?user_id=" + idTokeninfo.email, idToken).then((jobs) => {
       this.setState({
         numJobs: jobs.data.count ? jobs.data.count : 0,
       });
     });
+
     await httpGet("chats?email=" + idTokeninfo.email, idToken).then((chats) => {
       this.setState({
         numChats: chats.data.count ? chats.data.count : 0,
@@ -436,7 +437,7 @@ class Landing extends Component {
     });
   };
 
-  submitJob = () => {
+  submitJob = async () => {
     // check that all the required fields are set / properly set
     if (this.state.jobsData.job_tags.length > 3) {
       alert("There are more than 3 job tags selected.");
@@ -467,6 +468,11 @@ class Landing extends Component {
       return;
     }
 
+    let idToken = (await Auth.currentSession())
+      .getIdToken()
+      .getJwtToken()
+      .toString();
+
     // get user info and update jobs data
     const userProfile = JSON.parse(localStorage.getItem("userProfile"));
     var jobsDataObj = { ...this.state.jobsData };
@@ -475,11 +481,7 @@ class Landing extends Component {
     jobsDataObj.poster_given_name = userProfile.given_name;
 
     // post job and close popup
-    httpPost(
-      "jobs",
-      Auth.currentSession().getIdToken().getJwtToken(),
-      jobsDataObj
-    )
+    await httpPost("jobs", idToken, jobsDataObj)
       .then((res) => {
         this.props.enqueueSnackbar("Successfully submitted a job posting:", {
           variant: "success",
@@ -671,13 +673,16 @@ class Landing extends Component {
           >
             Purchase Credits
           </Button>
-          <Button
-            className={classes.button1}
-            variant="contained"
-            onClick={this.postJob}
-          >
-            Post a Job
-          </Button>
+          {jwtDecode(localStorage.getItem("idToken"))["custom:user_type"] !==
+          "FREE" ? (
+            <Button
+              className={classes.button1}
+              variant="contained"
+              onClick={this.postJob}
+            >
+              Post a Job
+            </Button>
+          ) : null}
           <EscalationsCard />
           <p className={classes.updateProfile}>Update your profile</p>
           <p className={classes.contact}>Contact Admin Support</p>
