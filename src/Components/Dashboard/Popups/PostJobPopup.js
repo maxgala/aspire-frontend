c30d4e380492c1e4e3e7073aa6a683c02ac6ad9b
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2020-12-25 22:22:38 -0500

    Allow users to enter beyond max characters, but show an error on post. (#301)

--- src/Components/Dashboard/Popups/PostJobPopup.js
+++ src/Components/Dashboard/Popups/PostJobPopup.js
@@ -190,8 +190,28 @@ class PostJobPopup extends Component {
 
   submitJob = async () => {
     // check that all the required fields are set / properly set
+    if (this.state.jobsData.description.length > this.state.max_characters) {
+      this.props.enqueueSnackbar(
+        "The job description exceeds the character limit of 2000 characters.",
+        {
+          variant: "warning",
+        }
+      );
+      return;
+    }
+    if (this.state.jobsData.requirements.length > this.state.max_characters) {
+      this.props.enqueueSnackbar(
+        "The job requirements exceeds the character limit of 2000 characters.",
+        {
+          variant: "warning",
+        }
+      );
+      return;
+    }
     if (this.state.jobsData.job_tags.length > 3) {
-      alert("There are more than 3 job tags selected.");
+      this.props.enqueueSnackbar("There are more than 3 job tags selected.", {
+        variant: "warning",
+      });
       return;
     }
     if (
@@ -250,13 +270,9 @@ class PostJobPopup extends Component {
     var jobsDataObj = { ...this.state.jobsData };
 
     if (id === "requirements") {
-      jobsDataObj.requirements = event.target.value
-        .toString()
-        .slice(0, this.state.max_characters);
+      jobsDataObj.requirements = event.target.value;
     } else if (id === "description") {
-      jobsDataObj.description = event.target.value
-        .toString()
-        .slice(0, this.state.max_characters);
+      jobsDataObj.description = event.target.value;
     } else if (id === "salary") {
       const re = /^[0-9\b]+$/;
       if (event.target.value === "" || re.test(event.target.value)) {
@@ -621,7 +637,6 @@ class PostJobPopup extends Component {
                   variant="outlined"
                   fullWidth
                   InputProps={{
-                    // maxLength: this.state.max_characters,
                     classes: {
                       root: classes.cssOutlinedInput,
                       focused: classes.cssFocused,
@@ -629,8 +644,7 @@ class PostJobPopup extends Component {
                     },
                   }}
                   value={this.state.jobsData.description}
-                  // helperText={`${this.state.jobsData.description.length}/${this.state.max_characters} Characters`}
-                  helperText={`${this.state.max_characters} Max Characters`}
+                  helperText={`${this.state.jobsData.description.length}/${this.state.max_characters} Characters`}
                   className={classes.textField}
                   onChange={this.handleFormDataChange("description")}
                 />
@@ -659,7 +673,6 @@ class PostJobPopup extends Component {
                   variant="outlined"
                   fullWidth
                   InputProps={{
-                    // maxLength: this.state.max_characters,
                     classes: {
                       root: classes.cssOutlinedInput,
                       focused: classes.cssFocused,
@@ -667,8 +680,7 @@ class PostJobPopup extends Component {
                     },
                   }}
                   value={this.state.jobsData.requirement}
-                  // helperText={`${jobsData.requirements.length}/${maxCharacters} Characters`}
-                  helperText={`${this.state.max_characters} Max Characters`}
+                  helperText={`${this.state.jobsData.requirements.length}/${this.state.max_characters} Characters`}
                   className={classes.textField}
                   onChange={this.handleFormDataChange("requirements")}
                 />
