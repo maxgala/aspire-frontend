0bc95be6472fa9f56a25bfe31b52709e910984cc
arsidada <arsalan.rana@gmail.com>
2021-01-22 18:36:53 -0500

    fixing lint errors

--- src/Components/Dashboard/Cards/CommunityCard.js
+++ src/Components/Dashboard/Cards/CommunityCard.js
@@ -12,7 +12,6 @@ import { withSnackbar } from "notistack";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogActions from "@material-ui/core/DialogActions";
 import Toolbar from "@material-ui/core/Toolbar";
 import close from "../../Images/close.png";
 
