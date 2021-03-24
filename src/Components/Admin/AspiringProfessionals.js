fb38599b2e187c4a5258730a7b07988ad4744ec8
Naba Sadia Siddiqui <NabaSadiaSiddiqui@users.noreply.github.com>
2021-03-23 21:38:58 -0400

    Update AspiringProfessionals.js

--- src/Components/Admin/AspiringProfessionals.js
+++ src/Components/Admin/AspiringProfessionals.js
@@ -164,6 +164,7 @@ class AspiringProfessionals extends Component {
       ) {
         let currentExecObject =
           existingAspiringProfessionalsData[aspiringProfessionalID].attributes;
+        console.log(currentExecObject);
 
         let aspiringProfessional = {};
         aspiringProfessional.name = `${currentExecObject.given_name} ${currentExecObject.family_name}`;
